39e388177bdff430ac0aaa4d5ba7999dfc717977
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2025-03-05 21:36:27 +0100

    update

--- js/01-default/windowsOnLoad.js
+++ js/01-default/windowsOnLoad.js
@@ -12,9 +12,16 @@ var year = document.getElementById('js-txt-year');
 // Body Footer: Year Element 
 var yearBodyFooter = document.getElementById('js-txt-year-copyright');
 
+// IT Docs Counter 
+var spanItDocsDone = document.getElementById('js-counter-it-doc-done');
+var spanItDocsInProgress = document.getElementById('js-counter-it-doc-in-progress');
+var spanItDocsComingSoon = document.getElementById('js-counter-it-doc-coming-soon');
+
+
 /* ------------------------------------------------------------ */
 /* Functions
 /* ------------------------------------------------------------ */ 
+
 
 /* ------------------------------------------------------------ */
 /* Events  
@@ -22,6 +29,7 @@ var yearBodyFooter = document.getElementById('js-txt-year-copyright');
 
 window.addEventListener( 'load', ()=> {
 
+    
     /* ---------------------------------------------- */
     /* Set Actually Date About
     /* ---------------------------------------------- */
@@ -29,6 +37,14 @@ window.addEventListener( 'load', ()=> {
 
     yearBodyFooter.innerText = getYear();
     
+    /* ------------------------------------------------------------------- */
+    /* Set Counted Number of It Docs, done, in progress, coming soon
+    /* ------------------------------------------------------------------- */
+
+    spanItDocsDone.innerText = document.querySelectorAll('#open-close-smooth-done-it-docs .it-doc-item-card').length;
+    spanItDocsInProgress.innerText =  document.querySelectorAll('#open-close-smooth-in-progress-it-docs .it-doc-item-card').length;
+    spanItDocsComingSoon.innerText =  document.querySelectorAll('#open-close-smooth-coming-soon-it-docs .it-doc-item-card').length;
+
     
 } )
 
