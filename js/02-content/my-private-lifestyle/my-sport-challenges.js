414b87bf7fed8c41867a21abd0ba7453c01ede55
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2025-03-12 19:00:15 +0100

    update

---
+++ js/02-content/my-private-lifestyle/my-sport-challenges.js
@@ -0,0 +1,18 @@
+console.log('[🟡|content|my-private-lifestyle| ➡️ my-sport-challenges.js]')
+
+
+/* --------------------------------------------------------- */
+/* Data Object | Sport Challenges
+/* --------------------------------------------------------- */
+
+var dataSportChallenges = [
+
+    {
+        a: "ddd"
+    }
+]
+
+
+console.log('🏃🏃🏃 = ' + dataSportChallenges[0].a)
+
+
