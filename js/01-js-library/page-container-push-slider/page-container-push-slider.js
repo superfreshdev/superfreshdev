dc7ada04aabf7014aed2eac4018f1850000b1bc2
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2025-08-07 17:01:00 +0200

    update

--- js/01-js-library/page-container-push-slider/page-container-push-slider.js
+++ js/01-js-library/page-container-push-slider/page-container-push-slider.js
@@ -1,6 +1,301 @@
 console.log("(🟨Lib) page-container-push-slider.js ")
 
 
-/* -------------------------------------------------------- */
-/* DOM Elements
-/* -------------------------------------------------------- */
+/* ------------------------------------------------------ */
+/* Function:
+/* > Handle correct Page Push Slide by Clicked Index
+/* ------------------------------------------------------ */
+
+// slides, currentIndex, nextIndex
+
+/*
+
+  1. Calc slide direction ( async resolve )
+  2. to slide
+
+*/
+
+
+  /*
+
+      1. upateIndexSlide
+      2. startPushSlide
+
+  */
+
+  /* ---------------------------------------------------------------------- */
+  /* Update Index Slide
+  /* ---------------------------------------------------------------------- */
+
+  function getNewIndexSlide( currentIndex, newIndex ) {
+
+    /* reminder:
+       check only new index slide
+    */
+
+    if( newIndex !== currentIndex ) {
+
+      return newIndex;
+
+    } else {
+
+      return -1;
+
+    }
+
+  }
+
+  /* ---------------------------------------------------------------------- */
+  /* Do Push Slide
+  /* ---------------------------------------------------------------------- */
+
+  function doPushSlide( slides, currentIndex, newIndex ) {
+
+
+    console.log("---------------------")
+    console.log("📏|slides.length= " + slides.length )
+    console.log("📏|currentIndex= " + currentIndex )
+    console.log("📏|newIndex= " + newIndex )
+    console.log("---------------------")
+
+    /* -------------------------------------------- */
+    /* Step 1: Style Slide Push Direction
+    /* -------------------------------------------- */
+
+    const currentPage = slides[currentIndex];
+    const nextPage = slides[newIndex]
+
+    // Multiplicator for Slide Direction
+    const direction = newIndex > currentIndex ? 1 : -1;
+
+    // Add Start Translate to 'nextPage'
+    nextPage.style.transition = 'none';
+    nextPage.style.transform = `translateX(${100 * direction}%)`;
+    nextPage.style.zIndex = 3;
+
+    // Fore Reflow ( Web-Browser )
+    // force Web-Browser to calculate
+    // the next Slide Element again to
+    // register the start transition
+    // why ?
+    // without that we lost / overwrite our
+    // start transition & in the end we dont get
+    // any transition
+    void nextPage.offsetWidth;
+
+    // add after new browser calc new transistion effect
+    currentPage.style.transition = 'transform .5s ease';
+    nextPage.style.transition = 'transform .5s ease';
+
+
+    /* -------------------------------------------- */
+    /* Step 2: Start Push Slide
+    /* -------------------------------------------- */
+
+    // Add End Translate to 'currentPage' from 'left to right' or 'right to left'
+    currentPage.style.transform = `translateX(${-100 * direction}%)`;
+
+    // Add End Translate to 'newPage' (100% -> 0% or -100% -> 0%)
+    nextPage.style.transform = 'translateX(0)';
+
+
+    /* -------------------------------------------- */
+    /* Step 3: Update New Slide
+    /* -------------------------------------------- */
+
+      // reminder: wait 500ms, then start
+      // the browser need some time to recalc by 'nextPage.offsetWidth'
+      // & do translate
+
+      setTimeout(() => {
+
+        /* ------------------------------------------------------------------ */
+        /* Step 1: Unshown Old Slide
+        /* ------------------------------------------------------------------ */
+
+        currentPage.classList.remove('active');
+
+        currentPage.style.transition = 'none';
+        currentPage.style.transform = 'translateX(100%)';
+        currentPage.style.zIndex = 1;
+
+        /* ------------------------------------------------------------------ */
+        /* Step 2: Show New Slide
+        /* ------------------------------------------------------------------ */
+
+        nextPage.classList.add('active');
+        nextPage.style.zIndex = 2;
+
+
+
+
+      }, 500)
+
+
+
+
+  }
+
+
+
+  /* ------------------------------------------------------- */
+  /* Function:
+  /* Get New Current Index
+  /* ------------------------------------------------------- */
+  function getNewClickedIndex ( currentIndex, clickedIndex ) {
+
+    console.log("🦍 currentIndex = " + currentIndex )
+    console.log("🦍 clickedIndex = " + clickedIndex )
+
+    if( clickedIndex != currentIndex ) {
+
+      console.log( " Next Index = " + clickedIndex )
+
+      return clickedIndex;
+
+    } else {
+
+      return -1;
+
+    }
+
+  }
+
+
+  async function goToSlide( slidePages, currentIndex , clickedIndex ) {
+
+    console.log( "Ich war drin" )
+
+    var currentIndex = currentIndex;
+    var nextIndex = clickedIndex;
+
+    /* ------------------------------------------------------ */
+    /* Step 1 | Handle only New Cklick
+    /* ----------------------------------------------------- */
+
+    if (nextIndex === currentIndex || nextIndex < 0 || nextIndex >= slidePages.length) return;
+
+    /* ------------------------------------------------------ */
+    /* Step 2 | Get Current & Next Page
+    /* ------------------------------------------------------ */
+
+    const currentPage = slidePages[currentIndex];
+    const nextPage = slidePages[nextIndex]
+
+    /* ------------------------------------------------------ */
+    /* Step 3 | Get Slide Direction 1=right,-1=left
+    /* ------------------------------------------------------ */
+
+    const direction = nextIndex > currentIndex ? 1 : -1;
+
+    /* ------------------------------------------------------ */
+    /* Step 4 | Get Dyn. Start Transition Position
+                for next Slide ( 100% = right / -100% = left )
+    /* ------------------------------------------------------ */
+
+    nextPage.style.transition = 'none';
+    nextPage.style.transform = `translateX(${100 * direction}%)`;
+    nextPage.style.zIndex = 3;
+
+    /* ------------------------------------------------------ */
+    /* Step 5 | Fore Reflow ( Web-Browser )
+                force Web-Browser to calculate
+                the next Slide Element again to
+                register the start transition
+                why ?
+                without that we lost / overwrite our
+                start transition & in the end we dont get
+                any transition
+    /* ------------------------------------------------------ */
+
+    void nextPage.offsetWidth;
+
+    /* ------------------------------------------------------ */
+    /* Step 6 | Add new transition to current & next page
+    /* ------------------------------------------------------ */
+
+    currentPage.style.transition = 'transform .5s ease';
+    nextPage.style.transition = 'transform .5s ease';
+
+    /* ------------------------------------------------------ */
+    /* Step 7 | Handle correct slide for current & next
+                - current can slide 100% or -100%
+                - next will only slide to 0, the showing page
+    /* ------------------------------------------------------ */
+
+    currentPage.style.transform = `translateX(${-100 * direction}%)`;
+    nextPage.style.transform = 'translateX(0)';
+
+    /* ------------------------------------------------------ */
+    /* Step 8 | Update new current index
+    /* ------------------------------------------------------ */
+
+    currentIndex = nextIndex;
+
+    /* ------------------------------------------------------ */
+    /* Step 9 | Wait 500ms, & then do new slide
+    /* ------------------------------------------------------ */
+
+    // reminder: do only return if the slide is finished
+
+    // const resolved = await start_push_slide( currentPage, nextPage );
+
+    // console.log("⁉️ = " + resolved )
+
+    if( "resolved" == await start_push_slide( currentPage, nextPage ) ) {
+
+      return currentIndex;
+
+    } else {
+
+      return -1;
+
+    }
+
+
+    // return await start_push_slide() ;
+
+  }
+
+
+
+
+  function start_push_slide( currentPage, nextPage ) {
+
+    return new Promise((resolve) => {
+
+      /* ----------------------------------------- */
+      /* Wait 500ms, then start push slide
+      /* ----------------------------------------- */
+      setTimeout(() => {
+
+        /* ------------------------------------------------------------------ */
+        /* Step 1: Unshown Old Slide
+        /* ------------------------------------------------------------------ */
+
+        currentPage.classList.remove('active');
+
+        currentPage.style.transition = 'none';
+        currentPage.style.transform = 'translateX(100%)';
+        currentPage.style.zIndex = 1;
+
+        /* ------------------------------------------------------------------ */
+        /* Step 2: Show New Slide
+        /* ------------------------------------------------------------------ */
+
+        nextPage.classList.add('active');
+        nextPage.style.zIndex = 2;
+
+        /* ------------------------------------------------------------------ */
+        /* Step 3: Send Resolve ( finish state )
+        /* ------------------------------------------------------------------ */
+        resolve("resolved");
+
+      }, 500)
+
+
+    })
+
+
+
+  }
--- js/03-content/header-body/header-body.js
+++ js/03-content/header-body/header-body.js
@@ -4,3 +4,147 @@ console.log("🟨 header-body.js ")
 /* -------------------------------------------------------- */
 /* DOM Elements
 /* -------------------------------------------------------- */
+
+const body_slides = document.querySelectorAll('.push-page-slide');
+const header_body_links =  document.querySelectorAll('.label-nav-header-body');
+
+/* ------------------------------------------------------ */
+/* Variables
+/* ------------------------------------------------------ */
+
+var currentIndex = 0;
+var nextIndex = 0;
+
+/* ------------------------------------------------------ */
+/* Functions
+/* ------------------------------------------------------ */
+
+// header_body_links[0].style.display = "none";
+
+/* ------------------------------------------------------ */
+/* Button Click | Event Listeners
+/* ------------------------------------------------------ */
+
+/*
+
+
+   ⚠️ Challenge
+
+   > Universell content-push-slider
+   > content-slide
+
+   > content-push-slider.js => Übergabe verarbeiten und CurrentIndex zurück geben ( async await )
+   > header-body.js => DOM Elements & Event Handler
+
+
+*/
+
+
+for( let clickedIndex=0; clickedIndex < header_body_links.length ; clickedIndex++ ) {
+
+
+  header_body_links[clickedIndex].addEventListener( 'click', () => {
+
+    console.log( "⚡ Link clicked = " + clickedIndex )
+
+    /* ----------------------------------------------------- */
+    /* Step 1: Check only New Clicked Index
+    /* ----------------------------------------------------- */
+
+    if( currentIndex != clickedIndex ) {
+
+
+      // console.log("🟩 do Slide ")
+      // console.log("currentIndex= " + currentIndex)
+      // console.log("clickedIndex= " + clickedIndex)
+
+
+      /* ----------------------------------------------------- */
+      /* Step 2: Do Push Slide
+      /* ----------------------------------------------------- */
+
+      // Async Await ... do Push Slide & if resolve than u allow to update currentIndex
+
+      doPushSlide( body_slides, currentIndex, clickedIndex )
+
+
+      /* ----------------------------------------------------- */
+      /* Step 3: Update new Current Index
+      /* ----------------------------------------------------- */
+
+      /*
+          ⚠️ Vielleicht braust du das garnicht
+          reminder:
+          after do push slide we allow to update new current index
+          because without async / promise
+      */
+
+        currentIndex = clickedIndex;
+
+      // setTimeout(() => {
+
+
+
+      // }, 2000)
+
+
+
+
+    }
+
+
+
+
+
+
+
+    // if index -1 do no slide because u clicked the same index
+
+
+
+
+
+
+    /* ------------------------------------ */
+    /* Async Await Solution
+    /* ------------------------------------ */
+
+    // Wait of new 'new current index' result
+    // active_index_header_body_link = goToSlide( body_slides, active_index_header_body_link, clickedIndex )
+
+
+
+    /* ------------------------------------ */
+    /* Timeout Solution
+    /* ------------------------------------ */
+
+    // active_index_header_body_link = goToSlide( body_slides , active_index_header_body_link , clickedIndex );
+
+    // setTimeout(() => {
+
+    //    console.log("(header-body.js) | active_index = " + active_index_header_body_link )
+
+    // }, 2000);
+
+
+  } )
+}
+
+
+/* ----------------------------------------------------------------- */
+/* Async Function
+/* ----------------------------------------------------------------- */
+
+async function async_go_to_slide( content_slides, currentIndex, clickedIndex ) {
+
+  console.log("async process > go to slide ... ➡️")
+
+  const new_current_index = await goToSlide( content_slides, currentIndex, clickedIndex );
+
+  console.log("async process > go to slide | Finished 🛑")
+
+  console.log("(header-body.js) | active_index = " + new_current_index )
+
+  return new_current_index;
+
+}
