42a60b8e4b3e360fae5f0b4dd0bbc9ff9e033f2f
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2025-09-14 18:56:28 +0200

    update

--- js/02_web_page/01_default/01_header/header-body.js
+++ js/02_web_page/01_default/01_header/header-body.js
@@ -9,17 +9,16 @@ console.log("🟡 header-body.js")
 /* DOM Elements
 /* -------------------------------------------------------- */
 
-const body_slides = document.querySelectorAll('#body-push-page-slider .push-content-slide');
-const header_body_links =  document.querySelectorAll('.label-nav-header-body');
-
-// radio of nav header
-const radios_body_header = document.querySelectorAll("input[name='name-radio-nav-header-body']");
+const body_page_slides = document.querySelectorAll("#body-push-page-slider .push-content-slide");
+const radios_body_header =  document.querySelectorAll("input[name='name-radio-nav-header-body']");
 
 /* ------------------------------------------------------ */
 /* Variables
 /* ------------------------------------------------------ */
 
 // default at the beginning
+// if currentIndex dont will be set by finding checked radio than
+// it will use this value of 0, that will be set the first page
 var currentIndex = 0;
 var nextIndex = 0;
 
@@ -33,53 +32,23 @@ var nextIndex = 0;
 /* Events
 /* ------------------------------------------------------ */
 
-/* ------------------------------------------------------ */
-/* Radio Change
-/* ------------------------------------------------------ */
+  /* ------------------------------------------------------ */
+  /* Add | Radio Change Listeners
+  /* ------------------------------------------------------ */
 
-for( let clickedIndex=0; clickedIndex < radios_body_header.length ; clickedIndex++ ) {
-
-
-  // add changek listener
-  radios_body_header[clickedIndex].addEventListener( 'change', () => {
-
-    console.log( "⚡ Radio Changed = " + clickedIndex )
-
-      /* ----------------------------------------------------- */
-      /* Step 1: Do Push Slide
-      /* ----------------------------------------------------- */
-
-      doPushSlide( body_slides, currentIndex, clickedIndex )
-
-      /* ----------------------------------------------------- */
-      /* Step 2: Update new Current Index
-      /* ----------------------------------------------------- */
-
-      currentIndex = clickedIndex;
+  for( let clickedIndex=0; clickedIndex < radios_body_header.length ; clickedIndex++ ) {
 
 
-  } )
+    // add changek listener
+    radios_body_header[clickedIndex].addEventListener( 'change', () => {
 
-}
-
-/* ------------------------------------------------------ */
-/* Execute
-/* ------------------------------------------------------ */
-
-    // Set the content by radio checked
-
-    for( let clickedIndex=0; clickedIndex < radios_body_header.length ; clickedIndex++ ) {
-
-
-    if( radios_body_header[clickedIndex].checked == true ) {
-
-        console.log("🟣🟣🟣 Start Page = " + clickedIndex )
+      console.log( "⚡ Radio Changed = " + clickedIndex )
 
         /* ----------------------------------------------------- */
         /* Step 1: Do Push Slide
         /* ----------------------------------------------------- */
 
-        doPushSlide( body_slides, currentIndex, clickedIndex )
+        doPushSlide( body_page_slides, currentIndex, clickedIndex )
 
         /* ----------------------------------------------------- */
         /* Step 2: Update new Current Index
@@ -87,6 +56,36 @@ for( let clickedIndex=0; clickedIndex < radios_body_header.length ; clickedIndex
 
         currentIndex = clickedIndex;
 
-    }
+
+    } )
+
+  }
+
+/* ------------------------------------------------------ */
+/* Execute (Relaod Web-Page)
+/* ------------------------------------------------------ */
+
+  // Set the content by find radio checked or set default index 0
+
+  for( let settedIndex=0; settedIndex < radios_body_header.length ; settedIndex++ ) {
+
+
+    if( radios_body_header[settedIndex].checked == true ) {
+
+        console.log("🟣🟣🟣 Start Page = " + settedIndex )
+
+        /* ----------------------------------------------------- */
+        /* Step 1: Do Push Slide
+        /* ----------------------------------------------------- */
+
+        doPushSlide( body_page_slides, currentIndex, settedIndex )
+
+        /* ----------------------------------------------------- */
+        /* Step 2: Update new Current Index
+        /* ----------------------------------------------------- */
+
+        currentIndex = settedIndex;
 
     }
+
+  }
