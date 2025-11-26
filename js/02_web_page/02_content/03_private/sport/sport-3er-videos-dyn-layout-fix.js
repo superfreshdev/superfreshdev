9bb2714b7786f99795ab5c4da1f79c44dfdd3f29
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2025-11-26 15:28:31 +0100

    updare

--- js/02_web_page/02_content/03_private/sport/sport-3er-videos-dyn-layout-fix.js
+++ js/02_web_page/02_content/03_private/sport/sport-3er-videos-dyn-layout-fix.js
@@ -6,10 +6,11 @@
 
   Problem:
   Grid dont delete row & col gap if there is only
-  one element - result is that we have an space
+  one element - bad view is that we have an space
   that we dont like to have
 
-  Solution: dyn. by java script to check if there is one or 2 elements
+  Solution: dyn. by java script to check if there is 1 or 2 elements
+  and then delete dyn. row or col gap css settings
 
 */
 
@@ -17,27 +18,73 @@
 /*  Var & Dom Elements
 /* ----------------------------------------------------------------- */
 
-const all_sport_3_videos_containers = document.querySelectorAll(".sport-3-videos-container");
-console.log("✳️ Lenght (3er) = " + all_sport_3_videos_containers.length )
+const sport_3_videos_containers = document.querySelectorAll(".sport-3-videos-container");
+console.log("✳️ Max Count (3er) = " + sport_3_videos_containers.length )
 
+// Only Dom Exist Count
+const childs_of_sport_3_videos = sport_3_videos_containers[1].querySelectorAll(".sport-video-link-box-3er-layout").length;
+console.log("✳️ Max Childs [1] (3er) = " + childs_of_sport_3_videos )
 
-// console.log("😡 Lenght (Divs) = " + all_sport_3_videos_containers[1].querySelectorAll("div").length )
-// change border
-// all_sport_3_videos_containers[1].querySelector("div:nth-of-type(1)").style.border = "10px solid red";
+// Only Dom Visible Count
+const visible_children = [...sport_3_videos_containers[1].children].filter(el =>
+  window.getComputedStyle(el).display !== "none");
 
-// 🟥 Problem: Find solution to get count child elements , internet says = filter by display none
+console.log(visible_children.length); // → 2
 
-// fixed
-// all_sport_3_videos_containers[1].style.columnGap = "0px";
-// all_sport_3_videos_containers[1].style.rowGap = "0px";
+/* ------------------------------ */
 
-//fixed
-// all_sport_3_videos_containers[2].style.rowGap = "0px";
-
-// console.log("🎻 Lenght (Divs) = " + all_sport_3_videos_containers[1].querySelectorAll(".sport-video-link-box-3er-layout").length )
-
-// console.log("🎻 Lenght (Divs) = " + all_sport_3_videos_containers[1].querySelectorAll(".sport-video-link-box-3er-layout").children.length )
+delete_row_col_gaps_by_grid_layouts( sport_3_videos_containers, ".sport-video-link-box-3er-layout" )
 
 /* ----------------------------------------------------------------- */
 /*  Functions
 /* ----------------------------------------------------------------- */
+
+function delete_row_col_gaps_by_grid_layouts( gridLayoutElements, cssChildElementSelector ) {
+
+  /* ----------------------------------- */
+  /* Step 1/1
+  /* ----------------------------------- */
+  /*
+      Count visible child elements & it works too,
+      if on the DOM is for example only 1 created child element
+  */
+
+  const all_grid_layouts = gridLayoutElements;
+
+  console.log("=> 🐦‍🔥 " + cssChildElementSelector )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Solution filter = display = none und dont exist
+
+// manipuliate
+// all_sport_3_videos_containers[1].querySelector("div").style.border = "10px solid red";
+
+// all_sport_3_videos_containers[1].style.backgroundColor = "red";
+// all_sport_3_videos_containers[1].style.rowGap = "0px";
+// all_sport_3_videos_containers[1].style.columnGap = "0px";
