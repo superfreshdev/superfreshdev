6f6b8bf0d1d90a521e7c549cbc79a9610e3660ae
DESKTOP-DVVSLFI\Marko <superfreshdev@gmx.de>
2025-11-28 00:04:37 +0100

    update

---
+++ js/01_js_library/01-utils/sets/set_radio.js
@@ -0,0 +1,29 @@
+console.log("🟨 set_radio.js")
+
+
+/* ----------------------------------------------------------------- */
+/* Set | First Radio if any sets
+/* ----------------------------------------------------------------- */
+
+function set_first_radio_by_any_sets( radios_name_group ) {
+
+  // console.log("🦧 set_first_radio_by_any_sets: ")
+
+  // Step 0:
+  // default check if radios exist
+  if ( exist_radios( radios_name_group ) == false ) {
+
+    console.log("🟥 radios dont exists")
+    return -1;
+
+  }
+
+  // Step 1:
+  // if any radio is checked,
+  // than set the first one
+  // console.log(" Set Modus")
+
+  const set_first_radio = document.querySelectorAll(`input[name='${radios_name_group}']`)[0];
+  set_first_radio.checked = true;
+
+}
--- js/02_web_page/02_content/03_private/sport/sport-category-handling.js
+++ js/02_web_page/02_content/03_private/sport/sport-category-handling.js
@@ -1,11 +1,172 @@
-// console.log("🟨 sport-category-handling.js")
+console.log("🟨 sport-category-handling.js")
 
+
+/*
+
+  to Dos:
+
+  1. auslagern - wiederverwendbarkeit
+  2. gezielte funktionen ansprechen
+
+  --------------------------------------------------
+
+  1. Update Selected Label
+  2. Update Header
+
+  3. Unset Radios
+
+*/
+
+/* ----------------------------------------------------------------- */
+/* Data Variables
+/* ----------------------------------------------------------------- */
+
+const data_sport_categories = {
+
+  defImgSrc: "",
+
+  sportCategories: [
+
+    {
+      imgName: "penis.png",
+      imgCss: "1.1em",
+      title: "Newest Videos"
+    },
+
+    {
+      imgName: "muschie.png",
+      imgCss: "1.1em",
+      title: "Challenges"
+    }
+
+  ]
+
+}
+
+const data_sport_newest_videos = {
+
+  defImgSrc: "",
+
+  videos: [
+
+    {
+      imgName: "aligator.png",
+      externalLink: "www.google.de"
+    },
+    {
+      imgName: "girffae.png",
+      externalLink: "www.google.de"
+    },
+    {
+      imgName: "elefant.png",
+      externalLink: "www.google.de"
+    }
+
+  ]
+
+}
+
+/* ----------------------------------------------------------------- */
+/* Variables & DOM Elements
+/* ----------------------------------------------------------------- */
+
+// Selected Sport Category ( label )
+const chk_selected_sport_category = document.getElementById("chk-selected-sport-category");
+const lbl_selected_sport_category = document.querySelector("label[for='chk-selected-sport-category']")
+
+const img_lbl_selected_sport_category = document.getElementById("js-img-selected-sport-category");
+const txt_lbl_selected_sport_category = document.getElementById("js-text-selected-sport-category");
+const txt_count_lbl_selected_sport_category = document.getElementById("js-countText-selected-sport-category");
+
+// Radios of Sport Categories ( newest videos, challenges )
+const txt_radio_name_sport_category = "radio-sport-category";
+
+var radios_sport_categories = [
+
+  document.getElementById("radio-sport-newest-videos"),
+  document.getElementById("radio-sport-challenge-videos"),
+
+]
+
+// Selected Sport Challenge Category ( label )
+
+// Radios of Sport Challenges Categories ( choose, ch1, ch2, ... )
+
+// Header Update
+
+/* ----------------------------------------------------------------- */
+/* Events
+/* ----------------------------------------------------------------- */
+
+// Step 2/2:
+// Add Radio Change Listener to close nav & update label & header
+
+for( let i=0; i < radios_sport_categories.length; i++ ) {
+
+  radios_sport_categories[i].addEventListener("change", ()=> {
+
+    console.log("Radio Changed State = " + i )
+
+    // Step 1/2
+    // close nav (chk unset)
+    chk_selected_sport_category.checked = false;
+
+    // Step 2/3
+    // update label
+
+    // Step 3/3
+    // update header
+    // Reminder: header is unshown by very small mobile size by display = none;
+
+  })
+
+
+}
+
+      // update_img_src_and_size( imgSelectedSportCategory, "media/img/icons/02-icon-colors/01-black/black-zick-zack-arrow.png", "1.1em")
+
+      // update_text( textSelectedSportCategory, "Newest Videos" )
+
+      // update_text( countTextSelectedSportCategory, "(2)" )
+
+
+// Radio Change | Sport Category Selections
+
+
+
+/* ----------------------------------------------------------------- */
+/* Execute
+/* ----------------------------------------------------------------- */
+
+// Step 1/2:
+// Set Default First Selection if not setted hard coded
+
+set_first_radio_by_any_sets( txt_radio_name_sport_category )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ----------------------------------------------------------------- */
+/* ❌❌❌ Trash
+/* ----------------------------------------------------------------- */
 
 /* ----------------------------------------------------------------- */
 /*  Update Action Element (img + text + count )
 /* ----------------------------------------------------------------- */
 
-
+// 🟣🟣🟣 Auslagern | Update Functions
 function update_img_src_and_size( imgElement, path, cssImgSize ) {
 
   imgElement.src = path;
@@ -88,6 +249,11 @@ for( let i=0; i < radios_sport_categories.length; i++ ) {
   }
 
 }
+
+
+
+
+
 
 /* ----------------------------------------------------------------- */
 /* Radio Change Event
